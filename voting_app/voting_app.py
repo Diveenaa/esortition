d6ba04dc05f052853d9b86f7b7929cd2a24bbeb3
lucian7701 <84269278+lucian7701@users.noreply.github.com>
2024-03-02 17:25:32 +0000

    feat: change name from voting_service to voting_app and decouple the database. This will now query a microservice to complete the tasks previously laid out

--- voting_app/voting_app.py
+++ voting_app/voting_app.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 import os
 
 
 app = Flask(__name__)
 # Configure the SQLALCHEMY_DATABASE_URI for your app before initializing the SQLAlchemy instance
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") 
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") 
 
 # Initialize the SQLAlchemy instance
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 # Define the Vote model
-class Vote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
-    age = db.Column(db.Integer)
-    vote = db.Column(db.String(80), unique=False, nullable=False)
+# class Vote(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=False, nullable=False)
+#     age = db.Column(db.Integer)
+#     vote = db.Column(db.String(80), unique=False, nullable=False)
 
 votes = {'yes': 0, 'no': 0}
 @app.route('/', methods=['GET', 'POST'])
@@ -28,8 +28,8 @@ def vote():
             votes['no'] += 1
     return render_template('voting.html')
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 
