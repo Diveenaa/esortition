ae877f792b0f9a061893afaaa989e1e2985dfa90
lucian7701 <84269278+lucian7701@users.noreply.github.com>
2024-03-02 17:03:58 +0000

    feat: voting web app as microservice with docker database instance

--- microservice2/micro_app.py
+++ microservice2/micro_app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_microservice():
-    return "hello microservice fdsafdsafdsa 2"
+    return "hello microservice fdsafd fdasfdasfdd safdsa 2"
---
+++ voting_service/inserts.sql
@@ -0,0 +1,15 @@
+-- mock data file
+INSERT INTO esortition_voting (username, age, vote) VALUES 
+('user1', 20, 'yes'),
+('user2', 23, 'no'),
+('user3', 24, 'yes'),
+('John', 25, 'yes'),
+('user4', 26, 'no'),
+('user5', 27, 'yes'),
+('user6', 28, 'no'),
+('user7', 29, 'yes'),
+('user8', 30, 'no'),
+('user9', 31, 'yes'),
+('user10', 32, 'no'),
+('user11', 33, 'yes'),
+('user12', 34, 'no')
