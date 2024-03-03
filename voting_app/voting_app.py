8c68bf739d24b6ce65046d43182d06c539619c00
lucian7701 <84269278+lucian7701@users.noreply.github.com>
2024-03-03 22:10:40 +0000

    feat: introduce nginx api gateway

--- voting_app/voting_app.py
+++ voting_app/voting_app.py
@@ -1,36 +1,36 @@
 from flask import Flask, request, render_template
-# from flask_sqlalchemy import SQLAlchemy
-# import os
+import requests
 
 
 app = Flask(__name__)
-# Configure the SQLALCHEMY_DATABASE_URI for your app before initializing the SQLAlchemy instance
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") 
 
-# Initialize the SQLAlchemy instance
-# db = SQLAlchemy(app)
+# votes = {'yes': 0, 'no': 0}
+# @app.route('/', methods=['GET', 'POST'])
+# def vote():
+#     if request.method == 'POST':
+#         vote = request.form['vote']
+#         if vote == 'yes':
+#             votes['yes'] += 1
+#         elif vote == 'no':
+#             votes['no'] += 1
+#     return render_template('voting.html')
 
-# Define the Vote model
-# class Vote(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=False, nullable=False)
-#     age = db.Column(db.Integer)
-#     vote = db.Column(db.String(80), unique=False, nullable=False)
+API_GATEWAY_URL = "http://nginx-api-gateway/voting/"
 
-votes = {'yes': 0, 'no': 0}
 @app.route('/', methods=['GET', 'POST'])
 def vote():
-    if request.method == 'POST':
-        vote = request.form['vote']
-        if vote == 'yes':
-            votes['yes'] += 1
-        elif vote == 'no':
-            votes['no'] += 1
-    return render_template('voting.html')
+    # if request.method == 'POST':
+    #     vote = request.form['vote']
+        # Here, instead of incrementing a local dict, you'd call the microservice
+        # to register the vote. This example just fetches data.
+        
+    # Fetch voter information from the voting_manager microservice
+    response = requests.get(API_GATEWAY_URL + "votes/")
+    if response.status_code == 200:
+        voters = response.json()  # Assuming the response is JSON containing voter data
+    else:
+        voters = []  # Handle errors or empty responses as needed
 
-# with app.app_context():
-#     db.create_all()
-
-
+    return render_template('voting.html', voters=voters)
 
 
